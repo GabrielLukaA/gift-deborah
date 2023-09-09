892b67ab96df806a8d79079771f0d12d9d432f71
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-09-09 16:00:55 -0300

    first try

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,54 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
+    <div className="flex w-screen pb-0 flex-col items-start">
+      <div className="flex w-[390px] px-4 pt-4 pb-0 flex-col items-center">
+        <div className="flex justify-between pt-8 pr-4 pl-0 pb-4 items-center self-stretch">
+          <div className="flex flex-col">
+            <h1 className="text-[#111] title-home">Seja bem-vinda,</h1>
+            <h1 className="text-[#111] title-home">Deborah!</h1>
+          </div>
+          <div className="flex px-3 py-2 items-center gap-2 rounded-lg border-purple default-shadow active:bg-[#901abb]">
+            {/* <img src="/moonPressed.svg" alt="" className="bg-[#901abb] relative hidden active:visible" /> */}
+            <img src="/moon.svg" alt="" className=""/>
+          </div>
+        </div>
+        <p className="paragraph-home text-[#272727]">
+          Se tudo ocorreu como planejado você ainda está vendo isso na data do
+          seu aniversário, então parabéns, eu espero que goste.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+      <div className="flex w-[390px] p-4 flex-col items-start gap-4">
+        <h2 className="text-[#111] medium-title">Menu</h2>
+        <div className="flex flex-col items-center gap-6">
+          <div className="flex w-[350px] p-4 flex-col items-start gap-4 rounded-2xl border-purple">
+            <h3 className="text-[#111] paragraph-16">Gerador de momentos!</h3>
+            <p className="text-[#272727] paragraph-home">
+              Bom, já se passou mais de um ano em que se vemos toda semana de
+              segunda à sexta, das 13:40 as 22:00. Talvez entrando aqui tenham
+              alguns momentos que pelo menos para mim foram divertidos, legais,
+              memoráveis, catastróficos, ou todas as opções anteriores.
+            </p>
+          </div>
+          <div className="flex w-[350px] p-4 flex-col items-start gap-4 rounded-2xl border-purple active:bg-[#901abb] active:text-[#FCFCFC]">
+            <h3 className="paragraph-16">Gerador de momentos!</h3>
+            <p className="paragraph-home">
+              Bom, já se passou mais de um ano em que se vemos toda semana de
+              segunda à sexta, das 13:40 as 22:00. Talvez entrando aqui tenham
+              alguns momentos que pelo menos para mim foram divertidos, legais,
+              memoráveis, catastróficos, ou todas as opções anteriores.
+            </p>
+          </div>
+          <div className="flex w-[350px] p-4 flex-col items-start gap-4 rounded-2xl border-purple">
+            <h3 className="text-[#111] paragraph-16">Gerador de momentos!</h3>
+            <p className="text-[#272727] paragraph-home">
+              Bom, já se passou mais de um ano em que se vemos toda semana de
+              segunda à sexta, das 13:40 as 22:00. Talvez entrando aqui tenham
+              alguns momentos que pelo menos para mim foram divertidos, legais,
+              memoráveis, catastróficos, ou todas as opções anteriores.
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
