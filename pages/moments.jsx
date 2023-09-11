2630a00e008cdd1fbbcb9cda9b00ca2725fbd0cf
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-09-10 22:34:26 -0300

    implementa carrinho

--- pages/moments.jsx
+++ pages/moments.jsx
@@ -1,13 +1,22 @@
 import Header from "../components/Header";
 export default function Mail() {
   return (
-    <div className="flex w-screen pb-0 flex-col items-start">
+    <div className="flex w-screen pb-0 flex-col items-start h-screen">
       <Header />
-      <div className="inline-flex h-[574px] p-[210px] flex-col justify-center items-center">
-        <div className="flex w-[342px] h-[138px] flex-col justify-between items-center shrink-0">
+      <div className="flex flex-col w-[390px] h-[60%] p-4 justify-center items-center">
+        <div className="flex flex-col gap-[78px]">
           <p className="medium-title">Gere agora uma nova memória</p>
-          <button className="flex h-9 p-2 justify-center items-center gap-2 shrink-0 self-stretch bg-[#901ABB] rounded-lg paragraph-16 text-[#fcfcfc]">Gerar</button>
+          <button className="flex h-9 p-2 justify-center items-center gap-2 self-stretch bg-[#901ABB] rounded-lg paragraph-16 text-[#fcfcfc]">
+            Gerar
+          </button>
         </div>
+      </div>
+      <div className="flex w-[390px] h-full pr-0 flex-col items-end">
+        <p className="title-home text-right">“Ser seu amigo é </p>
+        <p className="title-home text-right">de bom tom”</p>
+<div className="flex h-[70px] flex-col items-start absolute left-[-15px]  bottom-[-10px]">
+<img src="car.svg" alt="" />
+</div>
       </div>
     </div>
   );
